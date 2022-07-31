8592326d1fe191af759767149e507b220e879111
heeemin <101591389+heeemin@users.noreply.github.com>
2022-07-31 19:28:34 +0900

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -59,7 +59,7 @@ const Home = () => {
         events={[
           { daysOfWeek: [0, 1, 2, 3, 4, 5, 6], color: "white" }, //월화수목금토일-> +버튼 생성
         ]}
-        height="100%"
+        height="850px"
         width="100vw"
         eventContent={renderEventContent} //이벤트 내용 커스텀
         headerToolbar={{
